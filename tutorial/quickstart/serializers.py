ac1d9c399ee25f1e1ebaef3d672943de6a8ad3c8
Basichumans <36101600+Basichumans@users.noreply.github.com>
2022-07-11 15:06:49 +0300

    comments, comments count

--- tutorial/quickstart/serializers.py
+++ tutorial/quickstart/serializers.py
@@ -1,19 +1,6 @@
 from rest_framework import serializers
 from . import models
 
-
-class PostSerializer(serializers.ModelSerializer):
-    
-    user = serializers.ReadOnlyField(source='user.username')
-    user_id = serializers.ReadOnlyField(source='user.id')
-    
-    class Meta:
-        model = models.Post
-        fields = ('id', 'title', 'content', 'user', 'created_at', 'created_updated_at', 'user_id')
-        
-        
-
-
 class CommentSerializer(serializers.ModelSerializer):
     
     user = serializers.ReadOnlyField(source='user.username')
@@ -23,3 +10,21 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Comment
         fields = ('id', 'content', 'user', 'created_at', 'created_updated_at', 'user_id', 'post')
+
+class PostSerializer(serializers.ModelSerializer):
+    
+    user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+    # coments = CommentSerializer(many=True) # verry hungry for data size
+    # comments = serializers.StringRelatedField(many=True) # geriau nenaudoti 
+    comments = CommentSerializer(many=True , read_only=True)
+    comment_count = serializers.SerializerMethodField()
+    
+    def get_comment_count(self, obj):
+            return obj.comments.count()
+    class Meta:
+        model = models.Post
+        fields = ('id', 'title', 'content', 'user', 'created_at', 'created_updated_at', 'user_id', 'comments', 'comment_count')
+        
+        
+
